c80e94296d49400ed4884585a0c45e2951e4a9cf
Ziming Song <a1158288818@icloud.com>
2023-08-13 23:39:54 +0800

    Update Main.js

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Ilan Grapel's personal website.",
+  description: "Ziming Song's personal website.",
 };
 
 export default Main;
