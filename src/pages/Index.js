76f62a782a736e99c364ca3d866e9fa0bda504d7
Ziming Song <a1158288818@icloud.com>
2023-08-14 00:44:02 +0800

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,8 +12,7 @@ const Index = () => (
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, 
-            react application written with modern Javascript.
+            A beautiful,react application written with modern Javascript.
           </p>
         </div>
       </header>
