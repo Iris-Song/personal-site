f8ed513fe00d2be81fba22a3f66319a06727ffef
iris <a1158288818@icloud.com>
2023-09-05 10:55:08 -0400

    add youtube link

--- src/data/contact.js
+++ src/data/contact.js
@@ -3,7 +3,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 // import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-import { faYoutube } from '@fortawesome/free-regular-svg-icons/faYoutube';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
